712ce55b2a9216cdbe3705327e2429e5d1ddfde1
Michael Chew <MICH@milestonesys.com>
2021-01-04 10:05:39 +1100

    commit

--- Tools/findxml.ps1
+++
@@ -1,6 +0,0 @@
-$list = "c:\tmp2" , "c:\ArchiveDB" , "c:\Live2"
-
-foreach ($ii in $list) {
-	$item = Get-ChildItem -Path $ii -File archives_cache.xml -Recurse -Depth 5
-	write-host "Found in $ii - $($item.Directory.Name) $($item.Name) "
-}
